74628fb5a3dbaaa6fe021eef91b8d720e166ae93
Jonatan Schumacher <jonatan@jschumacher.com>
2015-10-07 09:46:56 -0400

    cleaned up properties.js

--- 01_properties/properties.js
+++ 01_properties/properties.js
@@ -14,28 +14,18 @@ function setPropertiesOnArrayObj(arr) {
 	arr.push(5);
 	arr.twoTimes = function(num) {
 		return num * 2;
-	}
+	};
 };
 
-// function setPropertiesOnFunctionObj(fun) {
-// 	fun.functionObject = function() {
-// 		return "I am a function object, all functions are objects! Function can have their own properties too!");
-// 	}
-// };
-
 function setPropertiesOnFunctionObj(fun) {
-	console.dir(fun);
-	// fun.functionObject = function() {
-	// 	return "yo";
-	// };
+	//console.dir(fun);
 
 	fun.year = 2015;
 	fun.divideByTwo = function(num) {
 		return num/2;
-	}
+	};
 
 	fun.prototype.helloWorld = function() {
 		return "Hello World";
 	};
 };
-
